5010dc3a7d06395f45649fefec3a299b5936dcc1
Jiri Sindelar <jorge.sindelar@gmail.com>
2024-07-02 08:44:31 +0200

    Backend - upload middleware - create folder if not exists

--- backend/src/middleware/file/upload.middleware.ts
+++ backend/src/middleware/file/upload.middleware.ts
@@ -1,15 +1,20 @@
 import { RequestHandler } from 'express';
 import multer from 'multer';
 import path from 'path';
+import fs from 'fs'; // Import fs module
 
 const uploadFolderPath = path.join(__dirname, '../../../public/uploads/');
 
 /**
  * Configure storage for file upload
+ * if the directory does not exist, create it
  */
 function configureStorage(uploadPath: string): multer.StorageEngine {
   return multer.diskStorage({
     destination: function (req, file, cb) {
+      if (!fs.existsSync(uploadPath)) {
+        fs.mkdirSync(uploadPath, { recursive: true });
+      }
       cb(null, uploadPath);
     },
     filename: (req, file, cb) => {
